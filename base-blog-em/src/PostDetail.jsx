cced2c32a4746ae922fbb9ac8789d2512d473a51
normuradoff <normuradoff1@gmail.com>
2021-09-04 17:52:27 +0500

    delete posts with useMutation hook

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery, useMutation } from 'react-query'
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,19 +25,37 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(['comments', post.id], () => fetchComments(post.id));
+
+  const deleteMutation = useMutation((postId) => deletePost(postId))
+
+  if (isLoading) {
+    return <h3>Loading comments...</h3>
+  }
+
+  if (isError) {
+    return <>
+      <h3>Something went wrong loading comments!</h3>
+      <p>{error.toString()}</p>
+    </>
+  }
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)} >Delete</button> <button>Update title</button>
+      {deleteMutation.isLoading && <p style={{ color: 'orange' }}>Deleting the post</p>}
+      {deleteMutation.isError && <p style={{ color: 'red' }}>Failed to delete the post</p>}
+      {deleteMutation.isSuccess && <p style={{ color: 'green' }}>The post has (not) been deleted</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
+      {
+        data.map((comment) => (
+          <li key={comment.id}>
+            {comment.email}: {comment.body}
+          </li>
+        ))
+      }
     </>
   );
 }
